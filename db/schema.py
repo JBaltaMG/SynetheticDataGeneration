834511680513af824da4182deb75182d65001fb6
rbaltser <rbaltser@kpmg.com>
2025-08-04 16:31:07 +0200

    tilføjet databasemodul

---
+++ db/schema.py
@@ -0,0 +1,12 @@
+schemadict = {
+    "dim_department": {
+        "department_id": "int",
+        "department_name": "str",
+        "proportionality": "float",
+    },
+    "dim_customer": {
+        "customer_id": "int",
+        "CustomerName": "str",
+        "CustomerSegment": "str",
+    },
+}
