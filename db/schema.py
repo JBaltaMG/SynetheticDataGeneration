df710ae6a72462e395a1a536d7b32d7f12f1c91a
rbaltser <rbaltser@kpmg.com>
2025-08-04 17:35:25 +0200

    opdatering databaseprojekt

--- db/schema.py
+++ db/schema.py
@@ -9,4 +9,21 @@ schemadict = {
         "CustomerName": "str",
         "CustomerSegment": "str",
     },
+    "dim_product": {
+        "product_id": "int",
+        "product_name": "str",
+        "product_category": "str",
+        "product_subcategory": "str",
+    },
+    "dim_account": {
+        "Account_ID": "int",
+        "AccountName": "str",
+        "AccountType": "str",
+    },
+    "dim_procurement": {
+        "ProcurementName": "str",
+    },
+    "dim_service": {
+        "ServiceName": "str",
+    },
 }
